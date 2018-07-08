fee98b7a88559e4b7928c0f5568bf1b5d7a851fd
Emilio Martinez <emiliomp25@gmail.com>
2018-07-07 22:41:28 -0400

    working on scrolling list

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var items = [];
+    document.getElementById("demo").innerHTML = items;
+
+    function myFunction() {
+        items.push("<br>Another Item");
+        document.getElementById("demo").innerHTML = items;
+    }
